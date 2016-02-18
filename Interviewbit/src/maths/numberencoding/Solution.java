580f9013566de2549cbb8caf97e399bfa30bbef7
Sravan Patti <Sravan@Sravans-MacBook-Pro.local>
2016-02-18 00:11:05 -0700

    Interviewbit: Math ARRANGE problem solved

--- Interviewbit/src/maths/numberencoding/Solution.java
+++ Interviewbit/src/maths/numberencoding/Solution.java
@@ -1,6 +1,7 @@
 package maths.numberencoding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Sravan on 2/17/16.
@@ -8,10 +9,29 @@ import java.util.ArrayList;
 public class Solution {
 
     public void arrange(ArrayList<Integer> a) {
-        
+        int N = a.size();
+
+        for(int i=0; i<N; i++){
+            int B = a.get(i);
+            int C = a.get(B);
+            if(C >= N) {
+                C = a.get(B)%N;
+            }
+            int encode = B + C*N;
+            a.set(i, encode);
+        }
+        for(int i=0; i<N; i++) {
+            a.set(i, a.get(i)/N);
+        }
+
     }
 
     public static void main(String... args) {
+
+        Solution solution = new Solution();
+        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1, 2, 0, 3));
+        solution.arrange(a);
+
 
     }
 }
