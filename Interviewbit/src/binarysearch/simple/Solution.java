5f7653ed9bd10abd73df7c6573358df89a937a8c
Sravan Patti <Sravan@Sravans-MacBook-Pro.local>
2016-02-20 17:10:48 -0700

    Checking in INSERTPOS Binary Search problem.

--- Interviewbit/src/binarysearch/simple/Solution.java
+++ Interviewbit/src/binarysearch/simple/Solution.java
@@ -51,18 +51,73 @@ public class Solution {
         return elementFound;
     }
 
+    //INSERTPOS
+    public int searchInsert(ArrayList<Integer> a, int b) {
+        int elementIndex = -1;
+        int low = 0;
+        int high = a.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if(b == a.get(mid)) {
+                elementIndex = mid;
+                return elementIndex;
+            }
+            else if(b < a.get(mid)) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        if(elementIndex == -1) {
+            low = 0;
+            high = a.size() - 1;
+            if(b > a.get(high)) {
+                return high+1;
+            }
+            else if(b < a.get(low)) {
+                return 0;
+            }
+            else {
+                while (low <= high) {
+                    int mid = low + (high - low)/2;
+                    if(b < a.get(mid)) {
+                        high = mid - 1;
+                    }
+                    else {
+                        low = mid + 1;
+                    }
+                }
+                if(b < a.get(low)) {
+                    return low;
+                }
+                else if(b > a.get(low) && b <a.get(high)) {
+                    return low + 1;
+                }
+                else {
+                    return high + 1;
+                }
+            }
+        }
+
+        return elementIndex;
+
+    }
+
     public static void main(String... args) {
         Solution solution = new Solution();
         ArrayList<ArrayList<Integer>> a = new ArrayList<ArrayList<Integer>>();
 
-        ArrayList<Integer> a1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
-        ArrayList<Integer> a2 = new ArrayList<Integer>(Arrays.asList(5, 6, 7, 8));
-        ArrayList<Integer> a3 = new ArrayList<Integer>(Arrays.asList(9, 10, 11, 12));
+        ArrayList<Integer> a1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 6, 7));
+        //ArrayList<Integer> a2 = new ArrayList<Integer>(Arrays.asList(5, 6, 7, 8));
+        //ArrayList<Integer> a3 = new ArrayList<Integer>(Arrays.asList(9, 10, 11, 12));
 
         a.add(a1);
-        a.add(a2);
-        a.add(a3);
+        //a.add(a2);
+        //a.add(a3);
 
-        System.out.println(solution.searchMatrix(a, 4));
+        //System.out.println(solution.searchMatrix(a, 4));
+        System.out.println(solution.searchInsert(a1, 5));
     }
 }
