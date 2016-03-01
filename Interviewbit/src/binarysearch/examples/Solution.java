88d921dd7d12f4aa4803bf94f541f980a4fd2dce
Sravan Patti <Sravan@Sravans-MacBook-Pro.local>
2016-02-29 22:47:26 -0700

    Checking in FINDMIN in rotated array Binary Search example problem.

--- Interviewbit/src/binarysearch/examples/Solution.java
+++ Interviewbit/src/binarysearch/examples/Solution.java
@@ -50,6 +50,35 @@ public class Solution {
         }
     }
 
+    // ROTATED ARRAY
+    public int findMin(final List<Integer> a) {
+        int low = 0;
+        int high = a.size()-1;
+
+        while(low <= high) {
+
+            if(a.get(low) <= a.get(high)) {
+                return a.get(low);
+            }
+
+            int mid = low + (high - low)/2;
+            int next = (mid + 1)%a.size();
+            int prev = (mid + a.size() - 1)%a.size();
+
+            if(a.get(mid) <= a.get(next) && a.get(mid) <= a.get(prev)) {
+                return a.get(mid);
+            }
+            else if(a.get(mid) <= a.get(high)) {
+                high = mid - 1;
+            }
+            else if(a.get(mid) >= a.get(low)) {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String... args) {
         Solution solution = new Solution();
         ArrayList<Integer> a = new ArrayList<>(Arrays.asList(2, 2, 2, 2, 3));
@@ -57,5 +86,11 @@ public class Solution {
         System.out.println(solution.findCount(a, 2));
         System.out.println(solution.findCount(a, 3));
         System.out.println(solution.findCount(a, 4));
+
+        ArrayList<Integer> b = new ArrayList<>(Arrays.asList(5, 4, 3, 1, 2));
+        ArrayList<Integer> c = new ArrayList<>(Arrays.asList(40342, 40766, 41307, 42639, 42777, 46079, 47038, 47923, 48064, 48083, 49760, 49871, 51000, 51035, 53186, 53499, 53895, 59118, 60467, 60498, 60764, 65158, 65838, 65885, 65919, 66638, 66807, 66989, 67114, 68119, 68146, 68584, 69494, 70914, 72312, 72432, 74536, 77038, 77720, 78590, 78769, 78894, 80169, 81717, 81760, 82124, 82583, 82620, 82877, 83131, 84932, 85050, 85358, 89896, 90991, 91348, 91376, 92786, 93626, 93688, 94972, 95064, 96240, 96308, 96755, 97197, 97243, 98049, 98407, 98998, 99785, 350, 2563, 3075, 3161, 3519, 4176, 4371, 5885, 6054, 6495, 7218, 7734, 9235, 11899, 13070, 14002, 16258, 16309, 16461, 17338, 19141, 19526, 21256, 21507, 21945, 22753, 25029, 25524, 27311, 27609, 28217, 30854, 32721, 33184, 34190, 35040, 35753, 36144, 37342
+        ));
+
+        System.out.println(solution.findMin(c));
     }
 }
