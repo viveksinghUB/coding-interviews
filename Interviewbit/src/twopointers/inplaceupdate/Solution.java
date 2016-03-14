39507ff1f5dca47f91c443678fb28283603cf65e
Sravan Patti <Sravan@Sravans-MacBook-Pro.local>
2016-03-14 00:01:59 -0600

    Checking in REMOVE_DUPLICATES_WITH_AT_MOST_2 elements from InPlaceUpdate section of Two Pointer.

--- Interviewbit/src/twopointers/inplaceupdate/Solution.java
+++ Interviewbit/src/twopointers/inplaceupdate/Solution.java
@@ -20,9 +20,31 @@ public class Solution {
         return index;
     }
 
+    public int removeDuplicates_WithAtMost2(ArrayList<Integer> a) {
+        int index = 1;
+        boolean addedTwice = false;
+        for(int i = 1; i < a.size(); i++){
+            if(!addedTwice && a.get(i).equals(a.get(i-1))) {
+                a.set(index, a.get(i));
+                addedTwice = true;
+                index++;
+            }
+            if(!a.get(i).equals(a.get(i-1))){
+                a.set(index, a.get(i));
+                addedTwice = false;
+                index++;
+            }
+        }
+
+        return index;
+    }
+
     public static void main(String... args) {
        Solution solution = new Solution();
         Integer[] a = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+        Integer[] b = {1, 1, 1, 1, 2, 2, 2};
         System.out.println(solution.removeDuplicates(new ArrayList<Integer>(Arrays.asList(a))));
+        System.out.println(solution.removeDuplicates_WithAtMost2(new ArrayList<Integer>(Arrays.asList(a))));
+        System.out.println(solution.removeDuplicates_WithAtMost2(new ArrayList<Integer>(Arrays.asList(b))));
     }
 }
