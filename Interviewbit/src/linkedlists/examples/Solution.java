60d3c91a7e2338bddc074179d736cde3be1b18d3
Sravan Patti <Sravan@Sravans-MacBook-Pro.local>
2016-04-20 21:41:23 -0600

    Checking test method for REVERSE_LIST from examples section of Linked Lists.

--- Interviewbit/src/linkedlists/examples/Solution.java
+++ Interviewbit/src/linkedlists/examples/Solution.java
@@ -80,6 +80,14 @@ public class Solution {
 
     public static void main(String... args) {
         Solution solution = new Solution();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        solution.reverseList(head);
     }
 }
 
